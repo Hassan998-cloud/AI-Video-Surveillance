897283ca73925db8fee6c875ef188cdda9fb0e6b
Hassan998-cloud <81471046+Hassan998-cloud@users.noreply.github.com>
2021-12-29 18:55:49 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 video = "";
+object = [];
 statuss = "";
 
 function preload(){
@@ -24,6 +25,28 @@ function modelLoaded(){
     video.volume(0);
 }
 
+function gotResult(error, results){
+    if (error){
+        console.log(error);
+    }
+    console.log(results);
+    object = results;
+}
+
 function draw(){
     image(video,0,0,480,380);
+    if (statuss != ""){
+        objectDetector.detect(video, gotResult);
+        for (i = 0 ; i < object.length ; i++){
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of Objects Detected are" + object.length; 
+            fill("#0062ff");
+            percent = floor(object[i].confidence * 100);
+            text(object[i].label + " " + percent + "%",object[i].x + 15, object[i].y + 15);
+            noFill();
+            stroke("#3cff00");
+            rect(object[i].x, object[i].y, object[i].width, object[i].height);
+        }    
+        }
+
 }
